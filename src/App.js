19d54b1388ead0c7a5bde235ec4e4ee38ede4a61
unknown <rocheanne041@gmail.com>
2022-03-13 13:31:01 +0800

    Added Movie Modal

--- src/App.js
+++ src/App.js
@@ -1,67 +1,75 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@mui/styles';
 import "./App.css";
 import HomeScreen from './screens/HomeScreen';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
 import PayPal from './screens/Paypal';
 import { auth } from './firebase';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { login, logout, selectUser } from './features/userSlice';
 import ProfileScreen from './screens/ProfileScreen';
 
 
 function App() {
-  const user = useSelector(selectUser);
-  const classes = useStyles();
-  //dispatch login or logout action
-  const dispatch = useDispatch();
-//user listen to login state
-  useEffect(() => {
-    //onAuthStateChanged - when detects someone logged in remember the state, access the user from any applistens to any authenticated change
-    //like a memory in browser it's sign in
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
-      if(userAuth) {
-        //Logged in
-        console.log(userAuth);
+    const user = useSelector(selectUser);
+    const classes = useStyles();
+    //dispatch login or logout action
+    const dispatch = useDispatch();
+    //user listen to login state
+    useEffect(() => {
+        //onAuthStateChanged - when detects someone logged in remember the state, access the user from any app listens to any authenticated change
+        //like a memory in browser it's sign in
+        const unsubscribe = auth.onAuthStateChanged(userAuth => {
+            if (userAuth) {
+                //Logged in
+                console.log(userAuth);
 
-        dispatch(login({
-          uid: userAuth.uid,
-          email: userAuth.email,
-        }))
-      } else {
-        //logged out
-        dispatch(logout())
-      }
-    });
-    //don't want to duplicate to another listener
-    //when it cleans up 
-    return unsubscribe;
-  }, [dispatch])
+                dispatch(login({
+                    uid: userAuth.uid,
+                    email: userAuth.email,
+                }))
+            } else {
+                //logged out
+                dispatch(logout())
+            }
+        });
+        //don't want to duplicate to another listener
+        //when it cleans up 
+        return unsubscribe;
+    }, [dispatch])
 
-  return (
-  <div className={classes.root}>
-<Router>
-{/* if there's no user it will go to login screen */}
-{!user ?(
-  <LoginScreen />) : 
-  (
-  <Routes>
-  <Route path = '/profile' element = {<ProfileScreen />} />
-  <Route path = '/checkout' element = {<PayPal /> } />
-  <Route path="/" element={<HomeScreen />} />
-  </Routes>
-)}
-</Router>
-  </div>
-  );
+    return ( <
+        div className = { classes.root } >
+        <
+        Router > { /* if there's no user it will go to login screen */ } {
+            !user ? ( <
+                    LoginScreen / > ) :
+                ( <
+                    Routes >
+                    <
+                    Route path = '/profile'
+                    element = { < ProfileScreen / > }
+                    /> <
+                    Route path = '/checkout'
+                    element = { < PayPal / > }
+                    /> <
+                    Route path = "/"
+                    element = { < HomeScreen / > }
+                    /> <
+                    /Routes>
+                )
+        } <
+        /Router> <
+        /div>
+    );
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    minHeight: "100vh",
-    backgroundColor: "#111"
-  }
+    root: {
+        minHeight: "100vh",
+        backgroundColor: "#111"
+    }
 }));
 
 
---
+++ src/components/MovieModal/MovieModal.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import './MovieModal.css';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/userSlice';
+import firebase from 'firebase';
+
+function MovieModal(props) {
+    const user = useSelector(selectUser).uid
+    const [loading, setLoading] = useState(null);
+    const [closeModal, setCloseModal] = useState(null);
+    console.log(user);
+
+    const addToMyList = (e) => {
+        let mounted = true;
+        setLoading(true);
+
+        firebase.database().ref('Account/' + user).push(props.aboutMovie).then(() => {
+            if (mounted) {
+                setLoading(false);
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+
+        return () => {
+            mounted = false;
+        }
+    }
+    return ( <
+        div className = { `movie--modal fadein ${closeModal ? 'close--div--modal' : null}` } >
+        <
+        div className = { `movie-modal-container ${closeModal ? 'close-modal' : null }` } >
+        <
+        div className = "header-movie-modal"
+        style = {
+            {
+                backgroundSize: 'cover',
+                backgroundImage: `url('https://image.tmdb.org/t/p/original/${props.aboutMovie.backdrop_path? props.aboutMovie.backdrop_path : props.aboutMovie.poster_path}'})`,
+                backgroundPosition: 'center center'
+            }
+        } <
+        div className = 'modal-shader' > < /div> <
+        svg version = '1.1'
+        onClick = {
+            () => setCloseModal(true) }
+        xlmns = 'http://www.w3.org/2000/svg'
+        x = '0px'
+        y = '0px'
+        viewBox = '0 0 13 13'
+        className = 'svg-icon-faq exit--btn--modal' >
+        <
+        polygon points = "7,6 7, 0 6, 0 6, 6 0, 6 0, 7 6, 7 6, 13 7, 13 7, 7 13, 7 13, 6 " > < /polygon> <
+        /svg> <
+        /div> {
+            loading ?
+                null :
+                <
+                div className = "fadein" >
+                <
+                h1 > {
+                    props.aboutMovie.title || props.aboutMovie.name || props.aboutMovie.originalbogo < /h1> <
+                    /div>
+
+                }
+        )
+
+    })
